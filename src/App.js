46120d14695f6298f6679b4c25aedf6661322ac6
Godfrey <ec2-user@ip-172-31-26-5.ec2.internal>
2021-01-24 05:23:29 +0000

    v3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './neural_network.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>How do you feel?</h1>
       </header>
     </div>
   );
